8779ff13a1c3709184124088cdb0602322fb9a62
rizkybiz <justin.defrank@hashicorp.com>
2022-01-27 09:46:29 -0500

    upd: added specific examples for retrieving URL from builds

--- examples/main.tf
+++ examples/main.tf
@@ -13,10 +13,24 @@ data "hashicorpversions_product" "product_version" {
   name = "consul"
 }
 
+locals {
+  version = data.hashicorpversions_product.product_version.version
+  builds = data.hashicorpversions_product.product_version.builds
+  arch_specific_builds = [for build in local.builds : build if build.arch == "${var.arch}"]
+}
+
 output "product_version" {
   value = data.hashicorpversions_product.product_version.version
 }
 
 output "product_builds" {
   value = data.hashicorpversions_product.product_version.builds
+}
+
+output "specific_arch_and_os" {
+  value = [for build in local.arch_specific_builds : build if build.os == "${var.os}" ]
+}
+
+output "specific_arc_and_os_url" {
+  value = element([for build in local.arch_specific_builds : build.url if build.os == "${var.os}" ],0)
 }
